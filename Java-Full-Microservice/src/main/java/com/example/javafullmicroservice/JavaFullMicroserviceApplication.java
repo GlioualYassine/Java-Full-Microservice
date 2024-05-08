7eb8213f268db6f296910400b22d73d65326db05
GlioualYassine <glioualyassine@gmail.com>
2024-05-08 21:39:43 +0100

    starting project

---
+++ Java-Full-Microservice/src/main/java/com/example/javafullmicroservice/JavaFullMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.javafullmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFullMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFullMicroserviceApplication.class, args);
+    }
+
+}
---
+++ Java-Full-Microservice/src/test/java/com/example/javafullmicroservice/JavaFullMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javafullmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFullMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
